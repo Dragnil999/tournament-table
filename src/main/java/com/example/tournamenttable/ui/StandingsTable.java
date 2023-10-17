2bd0cb7e29ccd3e6b1ed4530a4d6a92822d46cc0
danil <danilromanov999@gmail.com>
2023-10-17 15:59:30 +0700

    Добавил симметричное заполнение

--- src/main/java/com/example/tournamenttable/ui/StandingsTable.java
+++ src/main/java/com/example/tournamenttable/ui/StandingsTable.java
@@ -12,15 +12,39 @@ public class StandingsTable {
     public StandingsTable() {
         root = new VBox();
         updateTable(4, 4);
+        setListeners();
     }
+
     public void updateTable(int width, int height) {
         table = new TextField[width][height];
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
                 table[i][j] = new TextField();
+                if (i == j) {
+                    table[i][j].setDisable(true);
+                }
             }
         }
         setTableToNode();
+    }
+
+    private void setListeners() {
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                final TextField field = table[j][i];
+                table[i][j].textProperty().addListener((observableValue, oldValue, newValue) -> {
+                    field.setText(rotateString(newValue));
+                });
+            }
+        }
+    }
+
+    private String rotateString(String text) {
+        String[] buff = text.split(":");
+        if (buff.length == 1) {
+            return text;
+        }
+        return buff[1] + ":" + buff[0];
     }
 
     private void setTableToNode() {
@@ -30,6 +54,7 @@ public class StandingsTable {
         }
         root.getChildren().addAll(row);
     }
+
     public VBox getRoot() {
         return root;
     }
